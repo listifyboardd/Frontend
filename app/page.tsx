58f37a12d64f8b52127475d8a2fdc2f41f9fb05b
mukhailo <savcukmihajlo@gmail.com>
2024-12-09 14:32:26 +0200

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+import Footer from './layout/Footer'
+import Header from './layout/Header'
+
 function Home() {
   return (
-    <h1>
-      hello
-    </h1>
+    <>
+      <Footer></Footer>
+      <main>
+        <h1>Hello</h1>
+      </main>
+      <Header></Header>
+    </>
   )
 }
 
