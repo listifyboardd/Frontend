87e27b00c58e3f4922c4c03aad6dcce00da79b30
mukhailo <savcukmihajlo@gmail.com>
2024-12-10 11:43:00 +0200

    add prettier new rules

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import Footer from "./layout/Footer"
-import Header from "./layout/Header"
+import Footer from './layout/Footer';
+import Header from './layout/Header';
 
 function Home() {
-	return (
-		<>
-			<Footer></Footer>
-			<main>
-				<h1>Hello</h1>
-			</main>
-			<Header></Header>
-		</>
-	)
+  return (
+    <>
+      <Footer></Footer>
+      <main>
+        <h1>Hello</h1>
+      </main>
+      <Header></Header>
+    </>
+  );
 }
 
-export default Home
+export default Home;
