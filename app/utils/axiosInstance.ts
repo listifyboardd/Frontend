b0c4e95b5938dd019274cc81d6e142de578ab43d
ra334 <savcukmihajlo@gmail.com>
2024-12-14 20:01:10 +0200

    add env file

--- app/utils/axiosInstance.ts
+++ app/utils/axiosInstance.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
-import 'dotenv/config';
 
 const axiosInstance = axios.create({
   withCredentials: true,
   baseURL: process.env.BASE_URL,
 });
+
+console.log(process.env.BASE_URL);
 
 export default axiosInstance;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+import 'dotenv/config'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    BASE_URL: process.env.BASE_URL,
+  }
 };
 
 export default nextConfig;
