3ead5968630b55eba6a2039a799d900469a91b1b
mukhailo <savcukmihajlo@gmail.com>
2024-12-14 15:59:26 +0200

    add bunner component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Footer from './layout/Footer';
 import Header from './layout/Header';
+import Banner from './components/Banner';
 
 function Home() {
   return (
     <>
       <Header></Header>
+      <Banner></Banner>
       <Footer></Footer>
     </>
   );
