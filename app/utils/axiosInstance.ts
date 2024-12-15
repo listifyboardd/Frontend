0d51c92bbb2deef1c97be0f918960dc8a20baf98
ra334 <savcukmihajlo@gmail.com>
2024-12-15 15:53:46 +0200

    refresh access token

--- app/utils/axiosInstance.ts
+++ app/utils/axiosInstance.ts
@@ -5,6 +5,26 @@ const axiosInstance = axios.create({
   baseURL: process.env.BASE_URL,
 });
 
-console.log(process.env.BASE_URL);
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+      try {
+        await axiosInstance.post('/api/users/token/refresh/');
+        return axiosInstance(originalRequest);
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
