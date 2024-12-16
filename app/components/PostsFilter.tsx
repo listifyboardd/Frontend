641bc7776e7258043be296d0c1197ef6149d00d6
Mikhaylo Kisil <mihajlokisil20@gmail.com>
2024-12-16 22:58:54 +0200

    set up PostsFilter component

---
+++ app/components/PostsFilter.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { useState } from 'react';
+import Image from 'next/image';
+import searchIMG from '../../public/images/PostsFilter/dropdown.svg';
+
+function Search() {
+  return <div className=""></div>;
+}
+
+export default Search;
