db6c8356d3ece7c4f383cf523c4b7d2de2d1648f
mukhailo <savcukmihajlo@gmail.com>
2024-12-17 12:38:08 +0200

    update design

--- app/layout/Header.tsx
+++ app/layout/Header.tsx
@@ -1,24 +1,27 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import profileIMG from '../../public/images/header/profile.svg';
 import axiosInstance from '../utils/axiosInstance';
+import AuthLinks from '../components/AuthLinks';
 
 function Header() {
-  const [link, setLink] = useState('');
+  const [logged, setLogged] = useState<boolean>();
 
-  async function isLogged() {
-    const response = await axiosInstance.get('/api/users/user/');
-    if (response.status === 200) {
-      setLink('/profile');
-    } else {
-      setLink('/authorization');
+  useEffect(() => {
+    async function isLogged() {
+      try {
+        await axiosInstance.get('/api/users/user/');
+        setLogged(true);
+      } catch (e) {
+        setLogged(false);
+      }
     }
-  }
 
-  isLogged();
+    isLogged();
+  }, []);
 
   return (
     <div className="container">
@@ -27,19 +30,21 @@ function Header() {
           <Link href="/" className="font-kreadon font-semibold">
             ListiFyBoard
           </Link>
-          <nav className="flex gap-[24px]">
-            <ul className="flex items-center gap-[37px]">
-              <li className="hover:text-medium">
-                <Link href="/housing" className="hover:font-medium">
-                  Housing
-                </Link>
-              </li>
-              <li>
-                <Link href="/job" className="hover:font-medium">
-                  Job
-                </Link>
-              </li>
-            </ul>
+          <div className="flex gap-8">
+            <nav className="flex gap-[24px]">
+              <ul className="flex items-center gap-[37px]">
+                <li className="hover:text-medium">
+                  <Link href="/housing" className="hover:font-medium">
+                    Housing
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/job" className="hover:font-medium">
+                    Job
+                  </Link>
+                </li>
+              </ul>
+            </nav>
             <div className="flex items-center gap-[20px]">
               <Link
                 href="/create"
@@ -47,18 +52,24 @@ function Header() {
                 font-kreadon
                 bg-[#1A3C55]
                 text-white
+                text-[20px]
                 rounded-[24px]
-                py-[6px]
-                px-6
-              "
+                py-[9px]
+                px-6"
               >
                 Create new ad
               </Link>
-              <Link href={link} className="pr-4">
-                <Image src={profileIMG} alt="profile"></Image>
-              </Link>
+              <div className="">
+                {logged ? (
+                  <Link href="/profile">
+                    <Image src={profileIMG} alt="profile"></Image>
+                  </Link>
+                ) : (
+                  <AuthLinks />
+                )}
+              </div>
             </div>
-          </nav>
+          </div>
         </div>
       </header>
     </div>
