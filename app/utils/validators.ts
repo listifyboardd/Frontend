4d44b87d51a98deb0ddabb5384ddd74b9779025e
ra334 <savcukmihajlo@gmail.com>
2024-12-17 21:09:31 +0200

    change function email validator

--- app/utils/validators.ts
+++ app/utils/validators.ts
@@ -1,9 +1,6 @@
 export function validateEmail(email: string) {
-  return String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
 }
 
 export function isMinLength(password: string, minLength: number = 8) {
