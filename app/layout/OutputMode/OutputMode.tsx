3197303449aa33cea51243fe504dff9713f53847
ra334 <savcukmihajlo@gmail.com>
2024-12-19 17:41:28 +0200

    add output mode component

---
+++ app/layout/OutputMode/OutputMode.tsx
@@ -0,0 +1,35 @@
+'use client';
+import { useState } from 'react';
+import Switcher from '@/app/components/Switcher';
+
+interface OutputModeProps {
+  children: React.ReactNode;
+  title: string;
+  useSwitcher: boolean;
+}
+
+function OutputMode({ children, title, useSwitcher }: OutputModeProps) {
+  const [mode, setMode] = useState<string>('');
+
+  const test = children;
+
+  return (
+    <div className="mb-14">
+      {useSwitcher ? (
+        <div className="">
+          <Switcher value={mode} onChange={setMode} />
+        </div>
+      ) : null}
+      <div className="font-kreadon text-4xl font-semibold mb-8">{title}</div>
+      {mode == 'list' ? (
+        <div className="grid gap-y-6 m-auto max-w-[846px]">{test}</div>
+      ) : mode == 'grid' ? (
+        <div className="grid gap-y-6 gap-x-8 grid-cols-2 m-auto max-w-[1056px]">
+          {test}
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+export default OutputMode;
