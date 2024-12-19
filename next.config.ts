eedb087ec6e6799ff093100ac1abcdf24b787852
ra334 <savcukmihajlo@gmail.com>
2024-12-19 16:32:36 +0200

    add domen link for images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 import 'dotenv/config'
 
 const nextConfig: NextConfig = {
+  images: {
+    domains: ['listifyboard.com'],
+  },
+  
   env: {
     BASE_URL: process.env.BASE_URL,
   }
